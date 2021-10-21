8f65c3af07db416defa54273f64e87c2d03fa972
Indraneel <neel16112001@gmail.com>
2021-10-21 23:44:07 +0530

    Modified compose.ejs and set /compose route

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.set('view engine', 'ejs');
 
 const port = 3000;
 
+//Pushing the post objects into the posts
+const posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -32,7 +34,6 @@ app.listen(port, function () {
 app.get("/", function (request, myServerResponse) {
   myServerResponse.render('home', {
     homeContent: homeStartingContent,
-
   });
 
 });
@@ -47,4 +48,18 @@ app.get("/contact",function(request,myServerResponse){
   myServerResponse.render('contact',{
              contactContent : contactContent,
   });
+});
+
+app.get("/compose",function(request,myServerResponse){
+    myServerResponse.render('compose',{
+
+    });
+});
+
+app.post("/compose",function(request,myServerResponse){
+  let post = {
+    title : request.body.postTitle,
+    body : request.body.postBody,
+  };
+  posts.push(post);
 });
